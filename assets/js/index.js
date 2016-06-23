9f1a9ec615bc976b71439449d8496daf00db08ff
johnngugi <johnngugi777@gmail.com>
2016-06-23 16:23:12 +0300

    Changes to layout of homepage

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+$(document).on('ready', function() {
+      $(".regular").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      });
+      $(".center").slick({
+        dots: true,
+        infinite: true,
+        centerMode: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      });
+      $(".variable").slick({
+        dots: true,
+        infinite: true,
+        variableWidth: true
+      });
+    });
