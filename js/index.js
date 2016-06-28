357b91f035061da2609fe39e6d939d8a17367b4b
Harrisonkamau <kamauharrison87@gmail.com>
2016-06-28 12:59:13 +0300

    implement a datepicker

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ $(document).ready(function()
       $('.carousel').carousel({
         interval: 3000
       });
+       $("#datepicker").datepicker();
       $(window).load(function() {
         $('.flexslider').flexslider({
             selector: ".slides > div.tile",
